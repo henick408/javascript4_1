0430ab1e96335b694e5b2333a751c9d7e7cee126
henick408 <marcel.snopkowski@gmail.com>
2025-11-18 15:34:11 +0100

    4_1.3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const { isUndefined } = require('util');
 const app = express();
 
 // define endpoint for exercise 1 here
@@ -36,7 +37,33 @@ app.get('/math/rectangle/:width/:height', (req, res) => {
 
 
 
-//TODO3
+app.get('/math/power/:base/:exponent', (req, res) => {
+  const base = Number(req.params.base);
+  const exponent = Number(req.params.exponent);
+  const query = req.query;
+
+  console.log(base);
+  console.log(exponent);
+
+  if(isNaN(base) || isNaN(exponent)){
+    return res.status(400).json({error: "Proszę podać poprawne liczby."})
+  }
+
+  if(query.root == 'true' && base < 0){
+    return res.status(400).json({error: "Uzyskanie pierwiastka z liczby ujemnej jest niemożliwe."});
+  }
+
+  let result = {
+    result: Math.pow(base, exponent)
+  }
+
+  if(query.root == 'true'){
+    result.root = Math.sqrt(base);
+  }
+
+  res.json(result);
+
+});
 
 
 const PORT = process.env.PORT || 3000;
