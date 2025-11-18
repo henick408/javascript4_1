fb9ab163a0e5392fa47afdbc8655bcd1185fba08
Henick <marcel.snopkowski@gmail.com>
2025-11-18 11:28:36 +0100

    4_1.1

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const app = express();
 
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
-//TODO1  
-  res.json(result);
+  const r = Number(req.params.r);
+
+  if(isNaN(r) || r <= 0){
+    return res.status(400).json({error: "Musi być dodatnią liczbą"})
+  }
+
+  const result = {
+    area: Math.PI * r * r,
+    circumference: Math.PI * 2 * r
+  }
+
+  res.json(result); 
 });
 
 //TODO2
